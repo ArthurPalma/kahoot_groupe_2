85f1283ae2905e9a49895f52c82eaef3ac1af6fb
Elie Carrot <eyh.carrot@gmail.com>
2026-03-04 14:40:36 +0100

    fix environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDN10epSDpz2CpZ_fgv-2MyluyjGVAQQP8",
+    authDomain: "kahoot-repo.firebaseapp.com",
+    projectId: "kahoot-repo",
+    storageBucket: "kahoot-repo.firebasestorage.app",
+    messagingSenderId: "168631042618",
+    appId: "1:168631042618:web:46bb4a86c22ac2cfa7eb03"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "XX",
+    apiKey: "AIzaSyDN10epSDpz2CpZ_fgv-2MyluyjGVAQQP8",
     authDomain: "kahoot-repo.firebaseapp.com",
     projectId: "kahoot-repo",
     storageBucket: "kahoot-repo.firebasestorage.app",
     messagingSenderId: "168631042618",
-    appId: "YY"
+    appId: "1:168631042618:web:46bb4a86c22ac2cfa7eb03"
   }
 };
 

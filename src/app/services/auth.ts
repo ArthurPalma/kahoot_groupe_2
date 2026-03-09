6a887bb1971dae4e951711b94024b3d59d8c3527
Elie Carrot <eyh.carrot@gmail.com>
2026-03-09 17:01:17 +0100

    enhance registration error handling and toast message duration

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -32,14 +32,36 @@ export class AuthService {
     password: string,
     alias: string,
   ): Promise<void> {
-    const userCred = await createUserWithEmailAndPassword(
-      this.auth,
-      email,
-      password,
-    );
-    await this.userService.create({ alias, ...userCred.user });
-    await sendEmailVerification(userCred.user);
-    return this.logout();
+    try {
+      const userCred = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password,
+      );
+      await this.userService.create({ alias, ...userCred.user });
+      await sendEmailVerification(userCred.user);
+      return this.logout();
+    } catch (error: any) {
+      let message = `
+        Quelque chose s'est mal passé, veuillez vérifier vos informations 
+        ou réessayer plus tard.
+      `;
+      if (error.code === 'auth/email-already-in-use') {
+        message = `
+          Cette adresse email est déjà utilisée. Veuillez en choisir une autre.
+        `;
+      } else if (error.code === 'auth/invalid-email') {
+        message = `
+          L'adresse email fournie n'est pas valide. 
+          Veuillez vérifier et réessayer.
+        `;
+      }
+      const toast = await this.toastController.create({
+        message: message,
+        duration: 2000
+      });
+      await toast.present();
+    }
   }
 
   async login(email: string, password: string): Promise<void> {
@@ -53,14 +75,15 @@ export class AuthService {
       const name = userProfile?.alias || this.auth.currentUser?.email || '';
       toast = await this.toastController.create({
         message: `Connexion réussie, bienvenue ${name}!`,
-        duration: 1500,
+        duration: 2000,
       });
     } catch (error) {
       console.error(error);
+      console.warn("toto" + error);
       toast = await this.toastController.create({
         message: "Quelque chose s'est mal passé, veuillez vérifier"
           + " vos identifiants ou réessayer plus tard.",
-        duration: 1500,
+        duration: 2000,
       });
     } finally {
       await toast?.present();
@@ -76,7 +99,7 @@ export class AuthService {
     await sendPasswordResetEmail(this.auth, email);
     const toast = await this.toastController.create({
       message: 'Un email de réinitialisation de mot de passe a été envoyé.',
-      duration: 1500,
+      duration: 2000,
     });
     await toast.present();
   }
